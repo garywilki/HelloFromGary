f1ac45b785a9e02b21e807a219de90bb21a91f43
Dallin Suggs <dallin.suggs@gmail.com>
2021-10-07 16:59:58 -0600

    Send my greetings

--- src/wilki/Main.java
+++ src/wilki/Main.java
@@ -3,6 +3,9 @@ package wilki;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.print("Hello, my name is Gary.");
+
+        System.out.print("Hello, my name is Gary.");
+
+        System.out.print("\nHello Gary, my name is Dallin.");
     }
 }
